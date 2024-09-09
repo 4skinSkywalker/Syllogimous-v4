0336274591d6d0ab4659a814bc196994162f15a2
Fred's GitHub <stopchemtrailsfred@gmail.com>
2024-09-10 00:17:51 +0200

    deploy on github

--- src/app/modules/syllogimous/components/body/in-game/in-game.component.ts
+++ src/app/modules/syllogimous/components/body/in-game/in-game.component.ts
@@ -22,19 +22,23 @@ export class BodyInGameComponent implements OnInit, OnDestroy {
         const upperBound = 90;
         const lowerBound = 10;
         const bufferTime = 5;
-        this.timerFull = 90;
+        this.timerFull = upperBound;
 
         this.statsService.calcStats();
         const questionType = this.sylSrv.question.type;
         const questionPremises = this.sylSrv.question.premises.length;
         const typeBasedStats = this.statsService.typeBasedStats[questionType];
         if (typeBasedStats?.stats) {
-            const stats = typeBasedStats.stats[questionPremises];
-            if (stats && stats.count > 4) {
-                const avg = stats.sum / (1000 * stats.count);
-                this.timerFull = Math.max(lowerBound, Math.min(avg, upperBound)) + bufferTime;
-                console.warn("AVG for " + questionType + " and " + questionPremises + " premises is " + this.timerFull + " s")
+            const prevStats = typeBasedStats.stats[questionPremises - 1];
+            const currStats = typeBasedStats.stats[questionPremises];
+            let avg = 0;
+            if (currStats && currStats.count > 4) {
+                avg = currStats.sum / (1000 * currStats.count);
+            } else if (prevStats && prevStats.count > 4) {
+                avg = prevStats.sum / (1000 * prevStats.count) + (4 * bufferTime);
             }
+            this.timerFull = Math.max(lowerBound, Math.min(avg, upperBound)) + bufferTime;
+            console.warn("timerFull", this.timerFull);
         }
         
         this.timerLeft = this.timerFull;
