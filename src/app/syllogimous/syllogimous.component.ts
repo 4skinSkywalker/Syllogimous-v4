53297380eb005615eae2defd4c20d938f0cd3a61
Fred's GitHub <stopchemtrailsfred@gmail.com>
2024-11-26 00:03:06 +0100

    small stupid linear animation

--- src/app/syllogimous/syllogimous.component.ts
+++ src/app/syllogimous/syllogimous.component.ts
@@ -7,4 +7,17 @@ import { Component, HostBinding } from "@angular/core";
 })
 export class SyllogimousComponent {
     @HostBinding('style.backgroundImage') backgroundImage = `linear-gradient(62deg, #8EC5FC88 0%, #E0C3FC88 100%), url(assets/images/bg-${1 + Math.floor(Math.random() * 6)}.avif)`;
+
+    ngAfterViewInit() {
+        const greatTitle = document.querySelector(".great-title") as HTMLElement;
+        if (greatTitle) {
+            greatTitle.style.marginBottom = "-50px";
+            const t = setInterval(() => {
+                if (!parseInt(greatTitle.style.marginBottom)) {
+                    return clearInterval(t);
+                }
+                greatTitle.style.marginBottom = (parseInt(greatTitle.style.marginBottom) + 1) + "px";
+            }, 10);
+        }
+    }
 }
