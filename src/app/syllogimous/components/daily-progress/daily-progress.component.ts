9e39a77dfe9c208c51e8087518a2cb97e5405bd9
Fred's GitHub <stopchemtrailsfred@gmail.com>
2025-01-06 16:53:20 +0100

    Fuse daily and weekly progress bars together

--- src/app/syllogimous/components/daily-progress/daily-progress.component.ts
+++ src/app/syllogimous/components/daily-progress/daily-progress.component.ts
@@ -1,15 +1,12 @@
-import { Component, Input } from '@angular/core';
+import { Component } from '@angular/core';
 import { DailyProgressService } from '../../services/daily-progress.service';
 
 @Component({
     selector: 'app-daily-progress',
     templateUrl: './daily-progress.component.html',
-    styleUrls: ['./daily-progress.component.css']
+    styleUrls: ['./daily-progress.component.scss']
 })
 export class DailyProgressComponent {
-    @Input("showTimePlayedToday") showTimePlayedToday = false;
-    @Input("showWeeklyProgress") showWeeklyProgress = false;
-
     dailyProgressPercentage = 0;
     weeklyProgressPercentage = 0;
 
