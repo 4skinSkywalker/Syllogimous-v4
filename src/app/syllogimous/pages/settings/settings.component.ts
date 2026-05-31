e327d8fcd94cbb4c65733424e7ad99c9aca679df
Fred's GitHub <stopchemtrailsfred@gmail.com>
2026-05-31 20:08:56 +0200

    add color blindness support

--- src/app/syllogimous/pages/settings/settings.component.ts
+++ src/app/syllogimous/pages/settings/settings.component.ts
@@ -3,8 +3,17 @@ import { Router } from '@angular/router';
 import { EnumScreens } from '../../constants/syllogimous.constants';
 import { FormControl } from '@angular/forms';
 import { DEFAULT_DAILY_GOAL, DEFAULT_PREMISES_DOWN_THRESHOLD, DEFAULT_PREMISES_UP_THRESHOLD, DEFAULT_TRAINING_UNIT_LENGTH, DEFAULT_WEEKLY_GOAL, ProgressAndPerformanceService } from '../../services/progress-and-performance.service';
-import { LS_DAILY_GOAL, LS_PREMISES_DOWN_THRESHOLD, LS_PREMISES_UP_THRESHOLD, LS_TRAINING_UNIT_LENGTH, LS_WEEKLY_GOAL } from '../../constants/local-storage.constants';
+import { LS_COLOR_BLINDNESS_MODE, LS_DAILY_GOAL, LS_PREMISES_DOWN_THRESHOLD, LS_PREMISES_UP_THRESHOLD, LS_TRAINING_UNIT_LENGTH, LS_WEEKLY_GOAL } from '../../constants/local-storage.constants';
 import { SyllogimousService } from '../../services/syllogimous.service';
+
+export const loadColorBlindnessMode = () => {
+    const blindnessModeColor = localStorage.getItem(LS_COLOR_BLINDNESS_MODE);
+    if (blindnessModeColor) {
+        const [text, value] = blindnessModeColor.split(";");
+        console.log("Loaded color blindness mode:", {text, value});
+        document.documentElement.style.setProperty('--negated-color', value);
+    }
+};
 
 @Component({
     selector: 'app-settings',
@@ -21,6 +30,15 @@ export class SettingsComponent {
     trainingUnitLength = new FormControl(DEFAULT_TRAINING_UNIT_LENGTH);
     premisesUpThreshold = new FormControl(DEFAULT_PREMISES_UP_THRESHOLD);
     premisesDownThreshold = new FormControl(DEFAULT_PREMISES_DOWN_THRESHOLD);
+
+    colorBlindnessChoices = [
+        { text: "None", value: "rgb(128, 0, 0)" },
+        { text: "Protanopia", value: "rgb(73, 71, 0)", },
+        { text: "Deuteranopia", value: "rgb(80, 90, 0)" },
+        { text: "Tritanopia", value: "rgb(122, 0, 0)" },
+        { text: "Achromatopsia", value: "rgb(38, 38, 38)" }
+    ];
+    colorBlindnessMode = new FormControl(this.colorBlindnessChoices[0].value, { nonNullable: true });
 
     constructor(
         public router: Router,
@@ -52,6 +70,18 @@ export class SettingsComponent {
         this.premisesDownThreshold.setValue(premisesDownThreshold);
         this.premisesDownThreshold.valueChanges
             .subscribe(v => localStorage.setItem(LS_PREMISES_DOWN_THRESHOLD, String(v)));
+
+        const colorBlindnessMode = localStorage.getItem(LS_COLOR_BLINDNESS_MODE);
+        if (colorBlindnessMode) {
+            const [text, value] = colorBlindnessMode.split(";");
+            this.colorBlindnessMode.setValue(value);
+        }
+        this.colorBlindnessMode.valueChanges.subscribe(value => {
+            const text = this.colorBlindnessChoices.find(choice => choice.value === value)?.text;
+            const cmpKey = text + ";" + value;
+            localStorage.setItem(LS_COLOR_BLINDNESS_MODE, cmpKey);
+            loadColorBlindnessMode();
+        });
     }
 
 }
